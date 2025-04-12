a1f95ec9e716fe6be8c3cfe55aca61e88deea3d0
Dimitris <zinodimitris@gmail.com>
2025-04-12 15:33:54 +0200

    will not implement multiple runs for arima

--- methods/arima.py
+++ methods/arima.py
@@ -1,4 +1,5 @@
 import pmdarima as pm
+from pmdarima.arima import StepwiseContext
 import pandas as pd
 import logging
 import tqdm
@@ -39,26 +40,27 @@ class ArimaForecaster:
         self.model = None
         self.order = None
         
-
-    
-
+    # the results are the same, so we can avoid fitting multiple times
     def fit(self):
         """
         Trains the ARIMA model on the training data. Using AutoArima from pmdarima.
         """
         # logging.info(f"Training ARIMA model with order={self.order} and seasonal_order={self.seasonal_order}...")
-        adf = pm.arima.ndiffs(self.train_data, test='adf')
-        kpps = pm.arima.ndiffs(self.train_data, test='kpss')
-        
-        self.model = pm.auto_arima(
-            self.train_data,
-            start_p=1, 
-            start_q=1,
-            max_d= max(adf, kpps),
-            suppress_warnings=True,
-            stepwise=True,
-            error_action='ignore'
-        )
+        # adf = pm.arima.ndiffs(self.train_data, test='adf')
+        # kpps = pm.arima.ndiffs(self.train_data, test='kpss')
+        with StepwiseContext(max_steps=200):
+            self.model = pm.auto_arima(
+                self.train_data,
+                start_p=1, 
+                start_q=1,
+                max_d= 10,
+                max_p=10,
+                max_q=10,
+                suppress_warnings=True,
+                stepwise=True,
+                out_of_sample_size=10,
+                error_action='ignore'
+            )
         
         # self.model.fit(self.train_data)
         logging.info("ARIMA model training complete.")
@@ -93,11 +95,24 @@ class ArimaForecaster:
         final_forecast = pd.Series(forecasts, index=self.test_data.index, name=f"ARIMA Forecast (horizon={horizon})")
         return final_forecast
         
+# def experiment_n_times(n=10, train=None, test=None):
+#     if train is None or test is None:
+#         raise ValueError("train and test must be provided.")
+    
+#     for i in range(n):
+#         arima = ArimaForecaster(train_data=train, test_data=test)
+#         arima.fit()
+        
+
 
 if __name__ == "__main__":
-    data = pd.Series(range(100), index=pd.date_range(start='2023-01-01', periods=100, freq='D'))
-    train, test = data.iloc[:80], data.iloc[80:] # Train on first 15, test on last 5 (indices 15-19)
+    from utils.model_data_prep import prepare_data_for_modeling
+    train, test = prepare_data_for_modeling()
+    
+    train, test = prepare_data_for_modeling()
     arima = ArimaForecaster(train_data=train, test_data=test)
-    arima.fit()
-    arima.forecast(horizon=1)
+    # arima.fit()
+    # arima.forecast(horizon=1)
+    # arima.experiment_n_times(10)
+    # experiment_n_times(10, train, test)
     
