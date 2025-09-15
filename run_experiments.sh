9c29a8e27b95e81cebb415a2063d7b9e26e3ca23
Dimitris <zinodimitris@gmail.com>
2025-09-15 13:32:44 +0000

    modified experiments scripts

--- run_experiments.sh
+++ run_experiments.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
 for exp_name in train-restricted-years train-less-year-log train-less-year-linear; do
-  python scripts/experiments.py --method fm --exp_name "$exp_name" --timefreq 1d
+  python scripts/experiments.py --method naive --exp_name "$exp_name" --timefreq 1d
+done
+
+
+for exp_name in train-restricted-years train-less-year-log train-less-year-linear; do
+  python scripts/experiments.py --method arima --exp_name "$exp_name" --timefreq 1d
+done
+for exp_name in train-restricted-years train-less-year-log train-less-year-linear; do
+  python scripts/experiments.py --method sundial --exp_name "$exp_name" --timefreq 1d
 done
