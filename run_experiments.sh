2b474666e6884936aff1695575cc3423f9d0969c
Dimitris <zinodimitris@gmail.com>
2025-09-17 18:05:18 +0200

    chronos and sundial

--- run_experiments.sh
+++ run_experiments.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
 for exp_name in train-restricted-years train-less-year-log train-less-year-linear; do
-  python scripts/experiments.py --method naive --exp_name "$exp_name" 
-done
-for exp_name in train-restricted-years train-less-year-log train-less-year-linear; do
-  python scripts/experiments.py --method arima --exp_name "$exp_name" 
+  python scripts/experiments.py --method sundial --exp_name "$exp_name" 
 done
