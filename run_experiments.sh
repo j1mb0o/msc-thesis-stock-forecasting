ed3c880d14fd0c4d7863f2c01a3134c549ef2b5a
Dimitris <zinodimitris@gmail.com>
2025-09-17 16:48:58 +0200

    minor change

--- run_experiments.sh
+++ run_experiments.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
 for exp_name in train-restricted-years train-less-year-log train-less-year-linear; do
-  python scripts/experiments.py --method chronos_base --exp_name "$exp_name" --timefreq 1d
+  python scripts/experiments.py --method naive --exp_name "$exp_name" 
+done
+for exp_name in train-restricted-years train-less-year-log train-less-year-linear; do
+  python scripts/experiments.py --method arima --exp_name "$exp_name" 
 done
