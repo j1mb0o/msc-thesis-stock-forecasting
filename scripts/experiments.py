26eb4fa64f635732b156fa1b87565020516c80d2
Dimitris <zinodimitris@gmail.com>
2025-09-17 16:16:02 +0200

    more horizons for 1d preds

--- scripts/experiments.py
+++ scripts/experiments.py
@@ -62,18 +62,18 @@ tickers = ["MSFT"]
 timefreqs = ["1d"]
 
 if exp_name == "train-restricted-years":
-    # horizon_lens = [1, 5, 21, 63]
+    horizon_lens = [1, 5, 21, 63]
     horizon_lens = [1]
     train_last_ns = [int(value) for value in np.linspace(1, 10, 10)]
     days = False
 elif exp_name == "train-less-year-log":
-    # horizon_lens = [1, 5, 21, 63]
+    horizon_lens = [1, 5, 21, 63]
     horizon_lens = [1]
     log_spaces_values = np.logspace(np.log10(25), np.log10(250), 10)
     train_last_ns = [int(value) for value in log_spaces_values]
     days = True
 elif exp_name == "train-less-year-linear":
-    # horizon_lens = [1, 5, 21, 63]
+    horizon_lens = [1, 5, 21, 63]
     horizon_lens = [1]
     lineal_spaced_values = np.linspace(25, 250, 10)
     train_last_ns = [int(value) for value in lineal_spaced_values]
