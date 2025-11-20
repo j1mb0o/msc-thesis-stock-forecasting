25afb70d69478ce8f6cb2833c3d86a07d6b72e41
Dimitrios Kourntidis <66755426+j1mb0o@users.noreply.github.com>
2025-11-21 00:54:26 +0100

    Rerun rq2 (#17)
    * naive base results
    
    * naive pct results
    
    * arima base results
    
    * arima results pct
    
    * handle loading data in the new form
    
    * minor refactoring
    
    * sundial base results
    
    * sundial pct results
    
    * chronos base results
    
    * chronos pct results
    
    * Update experiments.py
    
    * naive results
    
    * arima results
    
    * modified for rq2
    
    * chronos results
    
    * sundial results
    
    * small refactor
    
    * sundial 1h results
    
    * naive 1h resilts
    
    * arima results 1h
    
    * fm results 1h
    
    * fm 1h results
    
    * chronos results 1h

--- scripts/experiments.py
+++ scripts/experiments.py
@@ -1,11 +1,21 @@
+import argparse
 import subprocess
 import time
 from itertools import product
+
 import numpy as np
-import argparse
 
 
-def run_pipeline(ticker, timefreq, method, horizon_len, train_last_n, exp_name, days_flag=False, pct_change=False):
+def run_pipeline(
+    ticker,
+    timefreq,
+    method,
+    horizon_len,
+    train_last_n,
+    exp_name,
+    days_flag=False,
+    pct_change=False,
+):
     """Runs the pipeline with the given parameters."""
     command = [
         "python",
@@ -28,56 +38,156 @@ def run_pipeline(ticker, timefreq, method, horizon_len, train_last_n, exp_name, 
     subprocess.run(command)
     # return command
 
-parser = argparse.ArgumentParser(description="Run experiments for time series forecasting.")
-parser.add_argument("--method", type=str, required=True, help="The forecasting method to use.")
-parser.add_argument("--exp_name", type=str, required=False, help="The name of the experiment to run.")
-parser.add_argument("--timefreq", default= "1d",type=str, required=False, help="The time frequency of the data.")
-parser.add_argument("--pct_change", action="store_true", help="Apply percentage change transformation to the data.")
+
+parser = argparse.ArgumentParser(
+    description="Run experiments for time series forecasting."
+)
+parser.add_argument(
+    "--method", type=str, required=True, help="The forecasting method to use."
+)
+parser.add_argument(
+    "--exp_name", type=str, required=False, help="The name of the experiment to run."
+)
+parser.add_argument(
+    "--timefreq",
+    default="1d",
+    type=str,
+    required=False,
+    help="The time frequency of the data.",
+)
+parser.add_argument(
+    "--pct_change",
+    action="store_true",
+    help="Apply percentage change transformation to the data.",
+)
 args = parser.parse_args()
 
 # TODO: remove tickers and have just a single variable, also remove tickers and timefreqs from for loop
 method = args.method
 exp_name = args.exp_name
 tickers = ["MSFT"]
-timefreqs = [args.timefreq]
+# timefreqs = [args.timefreq]
 
-for exp_name in ["train-restricted-years", "train-less-year-log", "train-less-year-linear"]:
-    print(exp_name)
 
-    if exp_name == "train-restricted-years":
-        horizon_lens = [1, 5, 21, 63]
-        # horizon_lens = [1]
-        train_last_ns = [int(value) for value in np.linspace(1, 10, 10)]
-        days = False
-    elif exp_name == "train-less-year-log":
-        horizon_lens = [1, 5, 21, 63]
-        # horizon_lens = [1]
-        log_spaces_values = np.logspace(np.log10(25), np.log10(250), 10)
-        train_last_ns = [int(value) for value in log_spaces_values]
-        days = True
-    elif exp_name == "train-less-year-linear":
-        horizon_lens = [1, 5, 21, 63]
-        # horizon_lens = [1]
-        lineal_spaced_values = np.linspace(25, 250, 10)
-        train_last_ns = [int(value) for value in lineal_spaced_values]
-        days = True
-    else:
-        raise NameError("The experiment name is not supported")
+# RQ 1
+def RQ1():
+    timefreqs = ["1d"]
+    for exp_name in [
+        "train-restricted-years",
+        "train-less-year-log",
+        "train-less-year-linear",
+    ]:
+        print(exp_name)
 
-    if args.pct_change:
-        exp_name = exp_name + "-pct"
+        if exp_name == "train-restricted-years":
+            horizon_lens = [1, 5, 21, 63]
+            # horizon_lens = [1]
+            train_last_ns = [int(value) for value in np.linspace(1, 10, 10)]
+            days = False
+        elif exp_name == "train-less-year-log":
+            horizon_lens = [1, 5, 21, 63]
+            # horizon_lens = [1]
+            log_spaces_values = np.logspace(np.log10(25), np.log10(250), 10)
+            train_last_ns = [int(value) for value in log_spaces_values]
+            days = True
+        elif exp_name == "train-less-year-linear":
+            horizon_lens = [1, 5, 21, 63]
+            # horizon_lens = [1]
+            lineal_spaced_values = np.linspace(25, 250, 10)
+            train_last_ns = [int(value) for value in lineal_spaced_values]
+            days = True
+        else:
+            raise NameError("The experiment name is not supported")
 
-    if method not in ["naive", "arima", "fm", "sundial", "chronos_base"]:
-        raise NameError("The method is not supported")
+        if args.pct_change:
+            exp_name = exp_name + "-pct"
 
-    total_exp = 0
+        if method not in ["naive", "arima", "fm", "sundial", "chronos_base"]:
+            raise NameError("The method is not supported")
 
-    for ticker, timefreq, horizon_len, train_last_n in product(tickers, timefreqs, horizon_lens, train_last_ns):
-        try:
-            run_pipeline(ticker, timefreq, method, horizon_len, train_last_n, exp_name, days_flag=days, pct_change=args.pct_change)
-            time.sleep(15)
-            total_exp += 1
-        except Exception as e:
-            print(f"Exception occured {e}")
-            exit()
-    print(total_exp)
+        total_exp = 0
+
+        for ticker, timefreq, horizon_len, train_last_n in product(
+            tickers, timefreqs, horizon_lens, train_last_ns
+        ):
+            try:
+                run_pipeline(
+                    ticker,
+                    timefreq,
+                    method,
+                    horizon_len,
+                    train_last_n,
+                    exp_name,
+                    days_flag=days,
+                    pct_change=args.pct_change,
+                )
+                time.sleep(15)
+                total_exp += 1
+            except Exception as e:
+                print(f"Exception occured {e}")
+                exit()
+        print(total_exp)
+
+
+# RQ 2
+def RQ2():
+    print(args.method)
+    timefreqs = ["1h"]
+    for exp_name in [
+        "train-restricted-years",
+        "train-less-year-log",
+        "train-less-year-linear",
+    ]:
+        print(exp_name)
+
+        if exp_name == "train-restricted-years":
+            # horizon_lens = [1, 5, 21, 63]
+            horizon_lens = [1]
+            train_last_ns = [int(value) for value in np.linspace(1, 10, 10)]
+            days = False
+        elif exp_name == "train-less-year-log":
+            # horizon_lens = [1, 5, 21, 63]
+            horizon_lens = [1]
+            log_spaces_values = np.logspace(np.log10(25), np.log10(250), 10)
+            train_last_ns = [int(value) for value in log_spaces_values]
+            days = True
+        elif exp_name == "train-less-year-linear":
+            # horizon_lens = [1, 5, 21, 63]
+            horizon_lens = [1]
+            lineal_spaced_values = np.linspace(25, 250, 10)
+            train_last_ns = [int(value) for value in lineal_spaced_values]
+            days = True
+        else:
+            raise NameError(f"The experiment {exp_name} is not supported")
+
+        if args.pct_change:
+            exp_name = exp_name + "-pct"
+
+        if method not in ["naive", "arima", "fm", "sundial", "chronos_base"]:
+            raise NameError("The method is not supported")
+
+        total_exp = 0
+
+        for ticker, timefreq, horizon_len, train_last_n in product(
+            tickers, timefreqs, horizon_lens, train_last_ns
+        ):
+            try:
+                run_pipeline(
+                    ticker,
+                    timefreq,
+                    method,
+                    horizon_len,
+                    train_last_n,
+                    exp_name,
+                    days_flag=days,
+                    pct_change=args.pct_change,
+                )
+                time.sleep(15)
+                total_exp += 1
+            except Exception as e:
+                print(f"Exception occured {e}")
+                exit()
+        print(total_exp)
+
+
+RQ2()
