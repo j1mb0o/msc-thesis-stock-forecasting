55e38c49a1a9e2f89b54a921739404f8d9addb28
Dimitris <zinodimitris@gmail.com>
2025-12-04 23:18:01 +0100

    unimprtant

--- misc/autoimport_tables.sh
+++ misc/autoimport_tables.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 #
-for file in ../tables/1d/train-restricted-years/*.tex; do echo "\input{$file}"; done
-echo ""
-
-for file in ../tables/1d/train-restricted-years-pct/*.tex; do echo "\input{$file}"; done
+for timefreq in 1d 1h; do
+    for subfolder in ../tables/$timefreq/*/; do
+        echo ""
+        echo "% $timefreq - $(basename "$subfolder")"
+        for file in "$subfolder"*.tex; do
+            if [ -f "$file" ]; then
+                echo "\input{$file}"
+            fi
+        done
+    done
+done
 
