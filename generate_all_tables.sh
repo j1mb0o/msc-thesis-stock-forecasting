238ad7220bd56490bbf5289fec99e5e1a8195289
Dimitris <zinodimitris@gmail.com>
2026-04-23 18:37:22 +0200

    re-gen all tables correctly

--- generate_all_tables.sh
+++ generate_all_tables.sh
@@ -12,29 +12,31 @@ YELLOW='\033[1;33m'
 NC='\033[0m' # No Color
 
 # Define experiment names
-# EXPERIMENTS=(
-# 	"rq3-covid-crash-2020-linear"
-# 	"rq3-covid-crash-2020-log-pct"
-# 	"rq3-financial-crisis-2008-log"
-# 	"rq3-market-downturn-2022-linear-pct"
-# 	"rq3-covid-crash-2020-linear-pct"
-# 	"rq3-financial-crisis-2008-linear"
-# 	"rq3-financial-crisis-2008-log-pct"
-# 	"rq3-market-downturn-2022-log"
-# 	"rq3-covid-crash-2020-log"
-# 	"rq3-financial-crisis-2008-linear-pct"
-# 	"rq3-market-downturn-2022-linear"
-# 	"rq3-market-downturn-2022-log-pct"
-# )
-
+# ==================== SOS VERY IMPORTANT PART ====================
 EXPERIMENTS=(
-    "train-less-year-linear"
-    "train-less-year-linear-pct"
-    "train-less-year-log"
-    "train-less-year-log-pct"
-    "train-restricted-years"
-    "train-restricted-years-pct"
+	"rq3-covid-crash-2020-linear"
+	"rq3-covid-crash-2020-log-pct"
+	"rq3-financial-crisis-2008-log"
+	"rq3-market-downturn-2022-linear-pct"
+	"rq3-covid-crash-2020-linear-pct"
+	"rq3-financial-crisis-2008-linear"
+	"rq3-financial-crisis-2008-log-pct"
+	"rq3-market-downturn-2022-log"
+	"rq3-covid-crash-2020-log"
+	"rq3-financial-crisis-2008-linear-pct"
+	"rq3-market-downturn-2022-linear"
+	"rq3-market-downturn-2022-log-pct"
 )
+
+# EXPERIMENTS=(
+#     "train-less-year-linear"
+#     "train-less-year-linear-pct"
+#     "train-less-year-log"
+#     "train-less-year-log-pct"
+#     "train-restricted-years"
+#     "train-restricted-years-pct"
+# )
+#================================================================
 
 # Define error metrics
 METRICS=(
